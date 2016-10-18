a2dca8974398bdde7cd053c2565d8f6ca64a1885
callum-hendry <callum.hendry@bluefruit.co.uk>
2016-10-18 16:28:24 +0100

    Setup Classes made, test project created with dummy failing test. Ready for code.

---
+++ Formatter_Tests/Formatter_Tests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Formatter_Tests
+{
+    [TestClass]
+    public class Formatter_Tests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int temp1 = 1, temp2 = 2;
+            Assert.AreEqual(temp1, temp2);
+        }
+    }
+}
---
+++ TeamCity_CSV_Formatter/Classes/ParseAndFormatLogicClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamCity_CSV_Formatter.Classes
+{
+    class ParseAndFormatLogicClass
+    {
+
+    }
+}
