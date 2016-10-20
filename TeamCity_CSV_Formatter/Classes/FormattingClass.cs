2247d8abc5d3f8c8890ea2aa3deddddadbed5b7c
callum-hendry <callum.hendry@bluefruit.co.uk>
2016-10-20 15:14:54 +0100

    Changed formatting tests to accomodate change of design, and removed one test that would require two different formatting functions (or one over-complicated version)

--- TeamCity_CSV_Formatter/Classes/FormattingClass.cs
+++ TeamCity_CSV_Formatter/Classes/FormattingClass.cs
@@ -8,10 +8,24 @@ namespace CSV_Formatter
 {
     public class FormattingClass
     {
-        public string[] Format(string[,] formattedCsv)
+        public string[] Format(string[,] parsedCsv)
+        {// 1st_section - format of input on test - just remove the _ and replace with " " to pass test
+            string[,] csvWithoutUnderscores = RemoveUnderscores(parsedCsv);
+
+            return csvWithoutUnderscores;
+        }
+
+        private string[,] RemoveUnderscores(string[,] parsedCsvWithoutUnderscores)
         {
-            string[] temp = { "" };
-            return temp;
+            for (int yCoordinate = parsedCsvWithoutUnderscores.GetLowerBound(0); yCoordinate <= parsedCsvWithoutUnderscores.GetUpperBound(0); ++yCoordinate)
+            {
+                for (int xCoordinate = parsedCsvWithoutUnderscores.GetLowerBound(1); xCoordinate <= parsedCsvWithoutUnderscores.GetUpperBound(1); ++xCoordinate)
+                {
+                    parsedCsvWithoutUnderscores[yCoordinate, xCoordinate] = parsedCsvWithoutUnderscores[yCoordinate, xCoordinate].Replace('_', ' ');
+                }
+            }
+
+            return parsedCsvWithoutUnderscores;
         }
     }
 }
