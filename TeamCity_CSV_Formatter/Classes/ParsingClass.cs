6cb62a629257e59013c2423de4cc2fabeb7464e2
callum-hendry <callum.hendry@bluefruit.co.uk>
2016-10-20 14:23:32 +0100

    Swapped "Assert" for "CollectionAssert" in tests, as "Assert" does not work for arrays. Also implemented ParsingClass.Parse()

--- TeamCity_CSV_Formatter/Classes/ParsingClass.cs
+++ TeamCity_CSV_Formatter/Classes/ParsingClass.cs
@@ -12,39 +12,17 @@ namespace CSV_Formatter
     {
         public string[,] Parse(string[] csvData)
         {
-            /* ~~~ Example code for iterating through a string's chars ~~~
-            string foo = "hello world", bar = string.Empty;
-            foreach(char c in foo)
-            {
-                bar += c;
-            }
-            */
             int lengthOfParsedCsvDataArray = 4; //Should match number of columns in CSV file
 
             string[,] parsedCsvData = new string[csvData.Length, lengthOfParsedCsvDataArray]; //[Y,X]
-            int xCoordinateOfArray = 0;
-            for (int i = 0; i < csvData.Length; i++)
+
+            for (int i = 0; i < csvData.Length; i++) //Y coordinate of parsedCsvData
             {
-                string stringBuffer = string.Empty;
                 string itemToParse = csvData[i];
-
-                foreach (char c in itemToParse) //Doesn't parse the 3rd_section part. Use a for loop with string.length as stop param. Can use string[j] as method to access chars in string.
+                string[] splitString = itemToParse.Split(',');
+                for (int j = 0; j < splitString.Length; j++)
                 {
-                    if (c == ',')
-                    {
-                        parsedCsvData[i, xCoordinateOfArray] = stringBuffer;
-                        stringBuffer = "";
-
-                        xCoordinateOfArray += 1;
-                        if (xCoordinateOfArray > (lengthOfParsedCsvDataArray - 1))
-                        {
-                            xCoordinateOfArray = 0;
-                        }
-                    }
-                    else
-                    {
-                        stringBuffer += c;
-                    }
+                    parsedCsvData[i,j] = splitString[j];
                 }
             }
 
