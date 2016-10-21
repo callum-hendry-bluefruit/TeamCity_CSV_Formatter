7fbb0ac90278302dbf08ec0243dd97b56eb9f156
callum-hendry <callum.hendry@bluefruit.co.uk>
2016-10-21 11:39:21 +0100

    Implemented part of Main(). Relative pathing to CSV location Skip first element of array as that is column titles Perform a check whether the CSV has been read
    Now checking output of parser and formatter, then print to text must be implemented

--- TeamCity_CSV_Formatter/Classes/FileAccessClass.cs
+++ TeamCity_CSV_Formatter/Classes/FileAccessClass.cs
@@ -3,18 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace CSV_Formatter
 {
     public class FileAccessClass
     {
-        public string[] ReadCsv()
-        {
-            string[] temp = { "" };
-            return temp;
+        public string[] ReadCsv(string pathToCsv)
+        { 
+            if (File.Exists(pathToCsv))
+            {
+                string[] csvData = File.ReadAllLines(pathToCsv);
+                return csvData;
+            }
+            else
+            {
+                string[] blank = { "NO_CSV" };
+                return blank;
+            }
         }
 
-        public void PrintToTxt(string textToPrint)
+        public void PrintToTxt(string[,] textToPrint)
         {
 
         }
--- TeamCity_CSV_Formatter/Program.cs
+++ TeamCity_CSV_Formatter/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace CSV_Formatter
 {
@@ -11,15 +12,27 @@ namespace CSV_Formatter
         static void Main(string[] args)
         {
             string[] csvData;
+            string[] unreadableCsv = { "NO_CSV" };
+            string pathToCsv = "../../../CoSimplex_ABSW_53-tests.csv";
 
             FileAccessClass AccessFile = new FileAccessClass();
-            csvData = AccessFile.ReadCsv();
+            csvData = AccessFile.ReadCsv(pathToCsv).Skip(1).ToArray(); //In addition to getting the CSV data, 
+                                                                       //it skips the 1st element of the array, 
+                                                                       //as that is not useful data.
+
+            if (csvData[0] == unreadableCsv[0])
+            {
+                MessageBox.Show("No CSV found. Press OK to exit", "ERROR");
+                Environment.Exit(404);
+            }
 
             ParsingClass CsvParser = new ParsingClass();
             string[,] seperatedCsv = CsvParser.Parse(csvData);
 
             FormattingClass parsedCsvFormatter = new FormattingClass();
             string[,] humanReadableCsv = parsedCsvFormatter.Format(seperatedCsv);
+
+            AccessFile.PrintToTxt(humanReadableCsv);
         }
     }
 }
